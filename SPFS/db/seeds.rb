ac911398847ae113dd0f81d739ac03ca33576c18
Josh Wykell <jwykell@gmail.com>
2016-06-04 18:01:43 -0400

    fixed search function

--- SPFS/db/seeds.rb
+++ SPFS/db/seeds.rb
@@ -1,27 +1,6 @@
 
-#
-<<<<<<< HEAD
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-#       businessname: name,
-#       firstname: row[1],
-#       lastname: row[2],
-#       phonenumber: phone,
-#       email: row[4],
-#       street: row[5],
-#       city: row[6],
-#       state: row[7],
-#       zip: row[8],
-#       description: description,
-#       url: row[10],
-#       active: true
-#     )
-#   end
-# end
 
-unless Provider.count > 0
+unless Provider.count > 1
   30.times do
     Provider.create!(
           businessname: Faker::StarWars.character,
@@ -39,4 +18,3 @@ unless Provider.count > 0
         )
   end
 end
->>>>>>> 8d61ef6313bcee297f66f8d36d0afec5ef4376a8
