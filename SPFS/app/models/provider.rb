8dd3fc99f22c907e1d9b1d98597aa6c79859098b
Steven Lee-Kramer <sleekramer@gmail.com>
2016-06-04 17:42:47 -0400

    seed file with faker for provider data

--- SPFS/app/models/provider.rb
+++ SPFS/app/models/provider.rb
@@ -1,10 +1,10 @@
 class Provider < ActiveRecord::Base
-  acts_as_taggable 
+  acts_as_taggable
   validates :businessname, presence: true
-  validates :phonenumber, presence: true
-  validates_format_of :phonenumber, with: /\A\d\d\d(-|\s)?\d\d\d(-|\s)?\d\d\d\d\z/
+  # validates :phonenumber, presence: true
+  # validates_format_of :phonenumber, with: /\A\d\d\d(-|\s)?\d\d\d(-|\s)?\d\d\d\d\z/
 
   def self.search(query)
-    where("businessname like ?", "%#{query}%") 
+    where("businessname like ?", "%#{query}%")
   end
 end
