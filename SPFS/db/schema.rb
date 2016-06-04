9dff76e22690427e95accc7669d7174916b392d7
Josh Wykell <jwykell@gmail.com>
2016-06-04 17:45:12 -0400

    changed type to provider type

--- SPFS/db/schema.rb
+++ SPFS/db/schema.rb
@@ -11,27 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604200030) do
+ActiveRecord::Schema.define(version: 20160604213219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "providers", force: :cascade do |t|
-    t.string   "businessname",                 null: false
+    t.string   "businessname",                  null: false
     t.string   "firstname"
     t.string   "lastname"
-    t.string   "phonenumber",                  null: false
+    t.string   "phonenumber",                   null: false
     t.string   "email"
     t.string   "street"
     t.string   "city"
     t.string   "state"
     t.string   "zip"
-    t.string   "description",                  null: false
+    t.string   "description",                   null: false
     t.string   "url"
-    t.boolean  "active",       default: false
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-    t.string   "type"
+    t.boolean  "active",        default: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.string   "provider_type"
   end
 
   create_table "taggings", force: :cascade do |t|
--- SPFS/db/seeds.rb
+++ SPFS/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Provider.create(businessname: 'Suzanne The Tutor', firstname: 'Suzanne', lastname: 'S.', city: 'St. Petersburg', state: 'FL', zip: '33706', description: 'I have over 7 years of experience in teaching ESOL to students from many countries. I have taught both large and small classrooms, one on one tutoring, and in home tutoring. I look forward to helping you expand your conversational and grammatical skills!
+
+I am looking for motivated students who would like to expand and strengthen their reading, writing, and grammatical skills. I feel that the relationship between teacher and student is also very important and strive to have the friendliest possible of connections.', active: true, provider_type: "ESL/ESOL")
